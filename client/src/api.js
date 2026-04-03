0d6b944ae86378f86e87c92e29978ce0745dd5a0
Luan Santos <luanjob2152@gmail.com>
2026-04-03 02:55:39 -0300

    t

--- client/src/api.js
+++ client/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
   // Já deixamos a URL base aqui para não precisar repetir
-  baseURL: "http://localhost:3001",
+  baseURL: "https://pride-barbers-api.onrender.com",
 });
 
 // Interceptor: Executa antes de QUALQUER requisição sair
